53bfae1da359b59ef36acf736680e3c145a9497a
Li Ting Ting <litingting@didapinche.com>
2018-03-13 15:19:44 +0800

    Initial Commit

---
+++ swipeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swipeDemo
+//
+//  Created by dida on 2018/3/13.
+//  Copyright © 2018年 given6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
